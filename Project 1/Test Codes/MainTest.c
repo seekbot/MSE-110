32db32e50bedd840d0e2410437f5b00dcd37db89
TarzaNGoblinN <shahvatsal2023@gmail.com>
2021-10-01 17:59:57 -0700

    Changed CS to Reflected
    Refer to lineTracking() function for specific changes to adjust to Reflected mode

--- Project 1/Test Codes/MainTest.c
+++ Project 1/Test Codes/MainTest.c
@@ -1,5 +1,5 @@
 #pragma config(Sensor, S1,     US,             sensorEV3_Ultrasonic)
-#pragma config(Sensor, S4,     CS,             sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S4,     CS,             sensorEV3_Color)
 #pragma config(Motor,  motorA,          leftWheel,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
 #pragma config(Motor,  motorB,          armMotor,      tmotorEV3_Medium, PIDControl, encoder)
 #pragma config(Motor,  motorD,          rightWheel,    tmotorEV3_Large, PIDControl, driveRight, encoder)
@@ -15,70 +15,71 @@ Sonar sensor - Port '1' and Color Sensor - Port '4'*/
 /*Global Variables*/
 
 /* Function List */
-void twoSecBeep();	 // 2 sec beep
-//void stop10();		 // stop 10 cm b4 obj
+void twoSecBeep();     // 2 sec beep
+//void stop10();         // stop 10 cm b4 obj
 void lineTracking(); // move while tracking line
 
 /* main func */
 task main()
 {
-	// set motor encoders to 0
-	nMotorEncoder[leftWheel] = 0;
-	nMotorEncoder[rightWheel] = 0;
+    // set motor encoders to 0
+    nMotorEncoder[leftWheel] = 0;
+    nMotorEncoder[rightWheel] = 0;
 
-	while (true)
-	{	// obstacle scenario
-		if (getUSDistance(US) <= 10)
-		{
-			// bot stops
-			setMotorSpeed(leftWheel, 0);
-			setMotorSpeed(rightWheel, 0);
+    while (true)
+    {    // obstacle scenario
+        if (getUSDistance(US) <= 10)
+        {
+            // bot stops
+            setMotorSpeed(leftWheel, 0);
+            setMotorSpeed(rightWheel, 0);
 
-			twoSecBeep();	// beeps for 2 sec
-			while(getUSDistance(US)>=5)
-			{
-				setMotorSpeed(leftWheel, 10);
-				setMotorSpeed(rightWheel,10);
-			}
-			while (getUSDistance(US) < 5)
-			{
-				moveMotorTarget(armMotor, 40, -50); // arms down
-				sleep(2000);
-				moveMotorTarget(armMotor, -80, -50); // arms up
-				sleep(2000);
-			}
-		}
+            twoSecBeep();    // beeps for 2 sec
+            while(getUSDistance(US)>=5)
+            {
+                setMotorSpeed(leftWheel, 10);
+                setMotorSpeed(rightWheel,10);
+            }
+            while (getUSDistance(US) < 5)
+            {
+                moveMotorTarget(armMotor, 40, -50); // arms down
+                sleep(2000);
+                moveMotorTarget(armMotor, -80, -50); // arms up
+                sleep(2000);
+            }
+        }
 
-		// line tracking
-		lineTracking();
-	}
+        // line tracking
+        lineTracking();
+    }
 }
 
 /* Function Def. */
 // 2 sec beep
 void twoSecBeep()
 {
-	for (int i = 1; i < 3; i++)
-	{
-		playSoundFile("Error alarm");
-		sleep(1000);
-	}
+    for (int i = 1; i < 3; i++)
+    {
+        playSoundFile("Error alarm");
+        sleep(1000);
+    }
 }
 
 void lineTracking()
 {
-	while(true)
-	{
-		if((getColorName(CS)==colorBlue)||(getColorName(CS)==colorGreen)||(getColorName(CS)==colorBlack))
-		{
-			setMotorSpeed(leftWheel, 45);
-			setMotorSpeed(rightWheel, 0);
-		}
-		//if no color detected, turn
-		else
-		{
-			setMotorSpeed(leftWheel, 0);
-			setMotorSpeed(rightWheel, 45);
-		}
-	}
+    while(true)
+    {
+        if(SensorValue[S4]<=10)
+        {
+            setMotorSpeed(leftWheel, 45);
+            setMotorSpeed(rightWheel, 25);
+        }
+        //if no color detected, turn
+        else
+        {
+            setMotorSpeed(leftWheel, 025);
+            setMotorSpeed(rightWheel, 45);
+        }
+     
+    }
 }
