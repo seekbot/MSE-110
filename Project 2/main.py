ea1cba0e1d92a7103e8d44c4e3a00c2d2d2a51f2
seekbot <70489385+seekbot@users.noreply.github.com>
2021-10-11 20:12:53 -0700

    Start of Project 2
    Line Tracking in EV3 MicroPython

---
+++ Project 2/main.py
@@ -0,0 +1,9 @@
+# libraries/imports
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+# functions
+
+# main
