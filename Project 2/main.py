a6c0ae8d3cda883b46e9a7e4e611d78b2766dbdb
Ji <ji01jeong@gmail.com>
2021-10-12 15:10:20 -0700

    Update Main.py
    Added Two Second Beep Function:
     - need to find the sound file directory
     - added a necessary library

--- Project 2/main.py
+++ Project 2/main.py
@@ -4,11 +4,13 @@ from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile
+from ev3dev2.sound import Sound
 
 # Initializing motors/sensors
 leftWheel = Motor(Port.A)
 rightWheel = Motor(Port.D)
 bot = DriveBase(leftWheel,rightWheel,wheel_diameter = ,axle_track = )
+# axle track: dist btwn pts where two wheels tocuh gnd
 
 US = UltrasonicSensor(Port.S1)
 CS = ColorSensor(Port.S4)
@@ -22,11 +24,22 @@ reflection = # light reflection btwn line and table
 LFPK = # constant to speed up correction (trial-n-error)
 speed = 100 # bot/wheel speed
 
+sound = Sound()
+sound.play_file('') # find 'Error alarm.wav' directory
+
 
 # functions
 def lineTracking():
 
+def twoSecBeep():
+    for i in range(0,2):
+        sound.beep()
+        sleep(1) # sleep for 1 sec (repeats twice - 2s)
+
 # main func.
+twoSecBeep()
+
+# line tracking (parameters required)
 while (true):
     correction = (reflection - CS.reflection()) * LFPK
     bot.drive(speed,correction)
