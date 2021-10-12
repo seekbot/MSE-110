e5bd094ec54b514863edecd96bba89c80cbb5dc2
Ji <ji01jeong@gmail.com>
2021-10-11 20:37:46 -0700

    Update main.py
    Initialized motors and sensors according to our first project bot.

--- Project 2/main.py
+++ Project 2/main.py
@@ -4,6 +4,19 @@ from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
-# functions
+# Initializing motors/sensors
+leftWheel = Motor(Port.A)
+rightWheel = Motor(Port.D)
+US = UltrasonicSensor(Port.S1)
+CS = ColorSensor(Port.S4)
 
-# main
+# global var.
+blue = 6
+green = 12
+bgColour = 35
+
+# functions
+def lineTracking():
+
+# main func.
+while (true):
