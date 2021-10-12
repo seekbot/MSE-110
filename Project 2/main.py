3f8f44c9920b27112f89b57068431ab0ea2e03c3
Ji <ji01jeong@gmail.com>
2021-10-12 09:16:25 -0700

    Updated Main.py
    Added more imports/libraries, codes

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,12 +1,15 @@
 # libraries/imports
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import Motor, ColorSensor, Ultrasonic Sensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile
 
 # Initializing motors/sensors
 leftWheel = Motor(Port.A)
 rightWheel = Motor(Port.D)
+bot = DriveBase(leftWheel,rightWheel,wheel_diameter = ,axle_track = )
+
 US = UltrasonicSensor(Port.S1)
 CS = ColorSensor(Port.S4)
 
@@ -15,8 +18,16 @@ blue = 6
 green = 12
 bgColour = 35
 
+reflection = # light reflection btwn line and table 
+LFPK = # constant to speed up correction (trial-n-error)
+speed = 100 # bot/wheel speed
+
+
 # functions
 def lineTracking():
 
 # main func.
 while (true):
+    correction = (reflection - CS.reflection()) * LFPK
+    bot.drive(speed,correction)
+
