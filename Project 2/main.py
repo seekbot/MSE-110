358cd837f55e5def485feaaec8702460ab7187f9
TarzaNGoblinN <shahvatsal2023@gmail.com>
2021-10-12 19:22:04 -0700

    Updated obstacle detection program
    Robot now stops and beeps for 2 sec

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 
 # libraries/imports
+from pybricks.hubs import *
 from pybricks.ev3devices import *
 from pybricks.parameters import *
 from pybricks.tools import wait
@@ -12,9 +13,9 @@ from pybricks.media.ev3dev import SoundFile
 leftWheel = Motor(Port.A)
 rightWheel = Motor(Port.D)
 robot = DriveBase(leftWheel,rightWheel,wheel_diameter = 55.5,axle_track = 104)
-
+ev3 = EV3Brick()
 us = UltrasonicSensor(Port.S1)
-us.mode = 'US-DIST-CM'    #put the ultrasonic sensor into distance mode
+cs = ColorSensor(Port.S4)
 
 # global var.
 blue = 6
@@ -24,33 +25,43 @@ bgColour = 35
 reflection = int((green + bgColour) / 2) # light reflection btwn line and table 
 LFPK = 2 # constant to speed up correction (trial-n-error)
 speed = 80 # bot/wheel speed
-objectDist = 10
-ev3=EV3Brick()
+objectDist = 10 # obstacle away from sensor
+
 
 # functions
+# tracks line
 def lineTracking():
         correction = (reflection - cs.reflection()) * LFPK
         robot.drive(speed,correction)
 
+# beeps for 2 sec
 def twoSecBeep():
     for i in range(0,2):
-        ev3.speaker.beep()
-        time.sleep(1) # sleep for 1 sec (repeats twice - 2s)
+       ev3.speaker.beep()
+       wait(1000) # sleep for 1 sec (repeats twice - 2s)
+
+# stop robot
+def stopMoving():
+      robot.stop()
+      leftWheel.brake()
+      rightWheel.brake()
 
 # main func.
 while True: #detects obstacle, proceeds to different colour functions
     
     lineTracking()
-    float dist_cm = us.value()/10 #change to cm from mm
+    dist_cm = us.distance()/10 # from mm to cm
 
     if dist_cm <= objectDist : #object detected within 10cm
-        twoSecBeep()
+        stopMoving() # stop 
+        twoSecBeep() # beep
         
-        if cs.value()<=blue :
         
-        elif cs.value()<=green : #if sensor detects a value thats equal to or less than green, 
+        #if cs.value() <= blue :
+        
+        #elif cs.value() <= green : #if sensor detects a value thats equal to or less than green, 
 
-        else: #if the sensor does not detect a colour value equal to or lower than green, the bot will turn for another colour value
+        #else: #if the sensor does not detect a colour value equal to or lower than green, the bot will turn for another colour value
 
     else: #does not detect obstacle, continues line tracking
         continue
