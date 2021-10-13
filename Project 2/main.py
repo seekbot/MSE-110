c6c995ee4f6f68a268f6a475c5b0fa006bd841de
TarzaNGoblinN <shahvatsal2023@gmail.com>
2021-10-12 18:35:19 -0700

    Made the code neater

--- Project 2/main.py
+++ Project 2/main.py
@@ -13,8 +13,11 @@ leftWheel = Motor(Port.A)
 rightWheel = Motor(Port.D)
 robot = DriveBase(leftWheel,rightWheel,wheel_diameter = 55.5,axle_track = 104)
 
-#us = UltrasonicSensor(Port.S1)
+us = UltrasonicSensor(Port.S1)
+us.mode = 'US-DIST-CM'    #put the ultrasonic sensor into distance mode
+
 cs = ColorSensor(Port.S4)
+cs.mode = 'COL-REFLECT'   #put colour sensor in reflective mode
 
 # global var.
 blue = 6
@@ -24,6 +27,8 @@ bgColour = 35
 reflection = int((green + bgColour) / 2) # light reflection btwn line and table 
 LFPK = 2 # constant to speed up correction (trial-n-error)
 speed = 80 # bot/wheel speed
+objectDist = 10
+
 
 # functions
 def lineTracking():
@@ -36,26 +41,19 @@ def twoSecBeep():
         time.sleep(1) # sleep for 1 sec (repeats twice - 2s)
 
 # main func.
-
-#put the ultrasonic sensor into distance mode
-us.mode='US-DIST-CM'
-
-#put colour sensor in reflective mode
-cs.mode='COL-REFLECT'
-
 while True: #detects obstacle, proceeds to different colour functions
+    
+    lineTracking();
+    float dist_cm = us.value()/10 #change to cm from mm
 
-    float dist_cm= us.value()/10 #change to cm from mm
-
-    if dist_cm<=10 : #object detected within 10cm
+    if dist_cm <= objectDist : #object detected within 10cm
         twoSecBeep()
         
         if cs.value()<=blue :
         
-        elif es.value()<=green : #if sensor detects a value thats equal to or less than green, 
+        elif cs.value()<=green : #if sensor detects a value thats equal to or less than green, 
 
         else: #if the sensor does not detect a colour value equal to or lower than green, the bot will turn for another colour value
 
-    
     else: #does not detect obstacle, continues line tracking
-        lineTracking()  # line tracking (parameters required)
+        continue
