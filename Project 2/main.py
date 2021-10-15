375f6e0c8eaae77d5ae84fb25c8bff3e3d998af3
seekbot <70489385+seekbot@users.noreply.github.com>
2021-10-14 17:47:42 -0700

    Updated main.py
    Updated the sharp turn and repositioning of line during obstacle detection

--- Project 2/main.py
+++ Project 2/main.py
@@ -21,10 +21,10 @@ cs = ColorSensor(Port.S4)
 # global var.
 blue = 6
 green = 12
-bgColour = 45
+bgColour = 35
 
-reflection = int((green + bgColour) / 2) # light reflection btwn line and table 
-LFPK = 2 # constant to speed up correction (trial-n-error)
+threshold = int((green + bgColour) / 2) # light reflection btwn line and table 
+deviation = 2 # constant to speed up correction (trial-n-error)
 speed = 80 # bot/wheel speed
 objectDist = 12 # obstacle away from sensor
 
@@ -43,17 +43,17 @@ def stopMoving():
 # tracks line
 def lineTracking():
     # tracks line
-    correction = (reflection - cs.reflection()) * LFPK
+    correction = (cs.reflection() - threshold) * deviation
     robot.drive(speed,correction)
+    isLeftExist = False 
     
     # sharp turns
-    if cs.reflection() >= bgColour:
+    if cs.reflection() > bgColour:
         resetWheels()
-        #stopMoving()
-        isLeftExist = False 
+        stopMoving()
         # search for path in left until detected
         while rightWheel.angle() < 720:
-            if cs.reflection() <= reflection:
+            if cs.reflection() <= green:
                 isLeftExist = True # path exists in left
                 stopMoving() 
                 resetWheels()
@@ -62,18 +62,19 @@ def lineTracking():
             else:
                 rightWheel.run(speed) # keep searching for line in left
 
-        # search for path in right until detected
-        while leftWheel.angle() < 720 and isLeftExist == False: 
-            if cs.reflection() <= reflection:
-                stopMoving()
-                resetWheels()
+    # search for path in right until detected
+    while leftWheel.angle() < 720 and isLeftExist == False: 
+        if cs.reflection() <= green:
+            stopMoving()
+            resetWheels()
 
-                robot.turn(45) # re-adjust the position of robot to the track
-                resetWheels()
-                break
+            robot.turn(45) # re-adjust the position of robot to the track
+            resetWheels()
+            break
 
-            else:
-                leftWheel.run(speed) # keep searching for the line in right
+        else:
+            leftWheel.run(speed) # keep searching for the line in right
+
 
 
 
@@ -89,26 +90,26 @@ while True: #detects obstacle, proceeds to different colour functions
     lineTracking()
     dist_cm = us.distance()/10 # from mm to cm
 
-    if dist_cm <= objectDist : #object detected within 10cm
-        stopMoving() # stop 
-        twoSecBeep() # beep
+    # if dist_cm <= objectDist : #object detected within 10cm
+    #     stopMoving() # stop 
+    #     twoSecBeep() # beep
         
-        #turn toward line 
-        robot.turn(-15)
-        # obstacle removing in blue
-        if cs.reflection() >= blue and cs.reflection() < green:
-            robot.turn(180)
+    #     #turn toward line 
+    #     robot.turn(-15)
+    #     # obstacle removing in blue
+    #     if cs.reflection() >= blue and cs.reflection() < green:
+    #         robot.turn(180)
 
-        # obstacle removing in green
-        elif cs.reflection() >= green and cs.reflection() < bgColour:  
-            while dist_cm > 3: # approaches the block until 3 cm away
-                robot.drive(speed, 0) 
-                dist_cm = us.distance()/10
-            stopMoving()
-            robot.turn(30)
-            robot.straight(500)
-            robot.straight(-500)
-            robot.turn(-30)
-        #doesn't detect=turns little by little to find line
-        else:
-            robot.turn(3)
+    #     # obstacle removing in green
+    #     elif cs.reflection() >= green and cs.reflection() < bgColour:  
+    #         while dist_cm > 3: # approaches the block until 3 cm away
+    #             robot.drive(speed, 0) 
+    #             dist_cm = us.distance()/10
+    #         stopMoving()
+    #         robot.turn(30)
+    #         robot.straight(500)
+    #         robot.straight(-500)
+    #         robot.turn(-30)
+    #     #doesn't detect=turns little by little to find line
+    #     else:
+    #         robot.turn(3)
