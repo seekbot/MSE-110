314b363a682340fb7413eef33952343768e54619
seekbot <70489385+seekbot@users.noreply.github.com>
2021-11-05 16:03:27 -0700

    Added a for loop for MinPeakHeight

--- Project 3/main.m
+++ Project 3/main.m
@@ -2,42 +2,10 @@ clc;
 clear all;
 close all;
 
-MyBarcode = xlsread('datalog-5.csv'); 
-%That is the values of the data in the picture are in format of 8 bit integers (maximum 255). 
-%This property will be transfered to any variables that we want to assign the value of image to that like MyRawImage
-%Therefore we need to change the variable type there after and for this reason we use the following command to change 
-%the variable types to double  
-OneLineData = double((MyBarcode(:,2)));
-% %Moving Average Filter with window size of ws
-for ws=7:14
-for i=1:length(OneLineData)-(ws-1)
-     OneLineDataAve(i)=sum(OneLineData(i:i+(ws-1)))/ws;
-end;
-
-% 
-for i=1:length(OneLineDataAve)-1
-    DataAveDif(i) = abs(OneLineDataAve(i+1) - OneLineDataAve(i));
-end
-% for iMinPeakHeight = 0.2:0.1:1.0
-[pks,locs] = findpeaks(DataAveDif,'MinPeakHeight',0.5,'MinPeakDistance',4);
-
-widths = (locs(2:end)-locs(1:end-1));
-widths = round(widths/min(widths));
-for i=1:length(widths)
-    if widths(i) >= 2
-        widths(i) = 3;  
-    end
-end
-% 
-% if length(widths)== 9
-%     break
-% end
-% end
-
 %**************Patern Recognition Using Lookup Table
 LOOKUPTABLE = [311113113  %A
                113113113  %B
-               313113111  %C
+               312113111  %C
                111133113  %D
                311133111  %E
                113133111  %F
@@ -63,14 +31,54 @@ LOOKUPTABLE = [311113113  %A
                133131111  %Z
                ]; 
            
+MyBarcode = xlsread('datalog-5.csv'); 
+OneLineData = double((MyBarcode(:,2)));
+
+% %Moving Average Filter with window size of ws
+for ws=7:14
+for i=1:length(OneLineData)-(ws-1)
+     OneLineDataAve(i)=sum(OneLineData(i:i+(ws-1)))/ws;
+end;
+
+% 
+for i=1:length(OneLineDataAve)-1
+    DataAveDif(i) = abs(OneLineDataAve(i+1) - OneLineDataAve(i));
+end
+
+for vMinPeakHeight = 0.2:0.1:1.8
+[pks,locs] = findpeaks(DataAveDif,'MinPeakHeight',vMinPeakHeight,'MinPeakDistance',4);
+
+widths = (locs(2:end)-locs(1:end-1));
+widths = round(widths/min(widths));
+
+for i=1:length(widths)
+    if widths(i) > 2
+        widths(i) = 3;  
+    else 
+        widths(i)=1;
+    end
+end
+
+if length(widths) == 9
+   break
+end
+end
+           
 CODE = str2num(strrep(num2str(widths), ' ', ''));
-  plot(DataAveDif)
+ ws
+ vMinPeakHeight 
+ widths
+ plot(OneLineData)
+ hold on
+ plot(OneLineDataAve)
+ figure;
+plot(DataAveDif)
   hold on 
   plot(locs,pks,'or')
   figure;
+  
   c = find(LOOKUPTABLE == CODE);
   Letter = char(64+c);
-  widths
   if c > 0
       plot(OneLineData)
       hold on
